385a886efa9cff2ab6a65f2f0489e99a44da23ec
Sergueille Bismuth <sergueille.bismuth@gmail.com>
2026-02-12 19:26:09 +0100

    Updated scripts

--- deploy_to_server.sh
+++ deploy_to_server.sh
@@ -4,9 +4,9 @@ echo "test version $(git rev-parse --short HEAD)" > client/version.txt
 # Backend
 cd server
 cargo build --release
-scp target/release/wardle-server root@\[2a09:6847:fa10:1410::278\]:/root/server/server
+scp target/release/wardle-server root@\[2a09:6847:fa10:1410::278\]:/opt/server/backend/server
 cd ..
 
 # Frontend
-scp -r client/* root@\[2a09:6847:fa10:1410::278\]:/root/client/www
-scp -r client/index.html root@\[2a09:6847:fa10:1410::278\]:/root/client/www/test.html
+scp -r client/* root@\[2a09:6847:fa10:1410::278\]:/opt/server/client/www
+scp -r client/index.html root@\[2a09:6847:fa10:1410::278\]:/opt/server/client/www/test.html
