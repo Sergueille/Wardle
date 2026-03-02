Sergueille/Wardle

commit c7c0e9839e65fb9b04a58378ac12b7d47fbaf77b

Client now connects to localhost server when url is localhost

client/index.js
@@ -1,5 +1,6 @@
 
-const DEFAULT_API_URL = "api.wardle.rezel.net";
+const DEFAULT_API_URL = "https://api.wardle.rezel.net";
+const LOCALHOST_API_URL = "http://localhost:4268";
 const API_PORT = 4268;
 
 const TEMPORARY_INFO_DELAY = 10000; //ms
@@ -124,7 +125,7 @@ function JoinRoom()
     state.roomCode = code;
 
     try {
-        let connection = new WebSocket("https://" + GetApiUrl() + "/join-room/" + code);
+        let connection = new WebSocket(GetApiUrl() + "/join-room/" + code);
 
         document.getElementById("join-room-btn").classList.add("connecting");
 
@@ -147,7 +148,7 @@ function JoinRoom()
 
 function CreateRoom() {
     try {
-        let connection = new WebSocket("https://" + GetApiUrl() + "/create-room");
+        let connection = new WebSocket(GetApiUrl() + "/create-room");
 
         ResetGlobalState();
         state.websocketConnection = connection;
@@ -483,7 +484,12 @@ function GetApiUrl() {
     return GetApiUrlWithoutPort();// + ":" + API_PORT.toString();
 }
 function GetApiUrlWithoutPort() {
-    return DEFAULT_API_URL;
+    if (window.location.host.indexOf("localhost") != -1) {
+        return LOCALHOST_API_URL;
+    }
+    else {   
+        return DEFAULT_API_URL;
+    }
 }
 
 function GetAllWords() {

commit 65eb531e51b7cc44102918a6b8cc34bb5b128ebb

Fixed reconnections
Previously broken by c7c0e98
Also added an additional toast

client/index.html
@@ -29,6 +29,9 @@
       <div class="toast error" id="room-join-failed">
         Couldn't join room. Make sure the code is correct. <!-- Impossible de rejoindre la salle. Assurer vous que le code est correct. -->
       </div>
+      <div class="toast" id="room-reconnecting">
+        Trying to reconnect...
+      </div>
       <div class="toast" id="room-reconnected">
         Connected again! <!-- Connexion rétablie ! -->
       </div>

client/index.js
@@ -403,7 +403,9 @@ function OnDisconnection() {
     setTimeout(() => { // Wait a little before reconnecting
         state.inReconnectionDelay = false;
 
-        let connection = new WebSocket("http://" + GetApiUrl() + "/reconnect/" + (state.isHostPlayer ? 0 : 1) + "/" + state.roomCode);
+        Toast("room-reconnecting");
+
+        let connection = new WebSocket(GetApiUrl() + "/reconnect/" + (state.isHostPlayer ? 0 : 1) + "/" + state.roomCode);
         state.websocketConnection = connection;
         
         connection.addEventListener("message", ev => HandleConnectionMessage(ev.data));